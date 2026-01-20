19c9c6b65842bd4df4441669a95961723ad2f6dd
leeeon233 <leeeon233@gmail.com>
2026-01-20 11:55:11 +0800

    fix: bin name

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use codex_common::CliConfigOverrides;
 fn main() -> Result<()> {
     arg0_dispatch_or_else(|codex_linux_sandbox_exe| async move {
         let cli_config_overrides = CliConfigOverrides::parse();
-        codex_acp::run_main(codex_linux_sandbox_exe, cli_config_overrides).await?;
+        acp_extension_codex::run_main(codex_linux_sandbox_exe, cli_config_overrides).await?;
         Ok(())
     })
 }
